e33b1eb99586d1da2a26fca58a8c2dad6d5c8025
Juan Manuel Beltrán Botello <jbeltrabo@unal.edu.co>
2026-08-06 09:50:10 -0500

    fix: optimizar puppeteer para prevenir error de memoria (OOM) en render

--- whatsapp-bot/index.js
+++ whatsapp-bot/index.js
@@ -16,7 +16,16 @@ console.log('Iniciando bot de WhatsApp...');
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ]
     }
 });
 
